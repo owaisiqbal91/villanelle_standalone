3f5b7a0b8d6e425f076469ec84e9acd452856d4f
Owais <owaisiqbal91@yahoo.com>
2019-02-04 19:45:07 -0500

    Added autocomplete and search features

--- src/components/villanelle_ace_editor.tsx
+++ src/components/villanelle_ace_editor.tsx
@@ -3,6 +3,10 @@ import AceEditor from 'react-ace';
 
 require("brace/mode/yaml");
 require("brace/theme/mono_industrial");
+require("brace/ext/language_tools");
+require('brace/ext/searchbox');
+var ace = require("brace");
+var langTools = ace.acequire("ace/ext/language_tools");
 
 export class VillanelleAceEditor extends React.Component<{code: string, handler: (string) => void}> {
 
@@ -16,6 +20,16 @@ export class VillanelleAceEditor extends React.Component<{code: string, handler:
     }
 
     render() {
+
+        var keywords = this.getKeywordsAutocompleteList();
+        var villanelleKeywordsCompleter = {
+            getCompletions: function(editor, session, pos, prefix, callback) {
+                callback(null, keywords);
+            }
+        }
+
+        langTools.setCompleters([langTools.textCompleter, villanelleKeywordsCompleter]);
+
         return <AceEditor
             mode="yaml"
             theme="mono_industrial"
@@ -29,6 +43,24 @@ export class VillanelleAceEditor extends React.Component<{code: string, handler:
             name="villanelle_ace_editor"
             value={this.props.code}
             editorProps={{ $blockScrolling: true }}
+            enableLiveAutocompletion={true}
         />
+    }
+
+    getKeywordsAutocompleteList() {
+        return [
+            { caption: "sequence:", name: "sequence:", value: "sequence:", meta: "" },
+            { caption: "selector:", name: "selector:", value: "selector:", meta: "" },
+            { caption: "condition:", name: "condition:", value: "condition:", meta: "" },
+            { caption: "effects:", name: "effects:", value: "effects:", meta: "" },
+            { caption: "Initialization:", name: "Initialization:", value: "Initialization:", meta: "" },
+            { caption: "ticks:", name: "ticks:", value: "ticks:", meta: "" },
+            { caption: "effect text:", name: "effect text:", value: "effect text:", meta: "" },
+            { caption: "User Interaction:", name: "User Interaction:", value: "User Interaction:", meta: "" },
+            { caption: "description:", name: "description:", value: "description:", meta: "" },
+            { caption: "action text:", name: "action text:", value: "action text:", meta: "" },
+            { caption: "effect tree:", name: "effect tree:", value: "effect tree:", meta: "" },
+            { caption: "user action:", name: "user action:", value: "user action:", meta: "" },
+        ];
     }
 }
